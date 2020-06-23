ec5840c78a785780e1ff2b2c27aa3b97af2d9842
Andrii Minchekov <andrii.minchekov@gmail.com>
2020-06-24 01:26:16 +0300

    Use jcache api to access apache ignite

--- order-service/src/main/kotlin/com/example/orderservice/hazelcast/DistributedOrderRepository.kt
+++ order-service/src/main/kotlin/com/example/orderservice/hazelcast/DistributedOrderRepository.kt
@@ -2,59 +2,39 @@ package com.example.orderservice.hazelcast
 
 import com.example.orderservice.domain.order.Order
 import com.example.orderservice.domain.order.OrderRepository
-import com.hazelcast.cache.impl.HazelcastServerCachingProvider
-import com.hazelcast.config.*
-import com.hazelcast.core.Hazelcast
-import com.hazelcast.core.HazelcastInstance
-import info.jerrinot.subzero.SubZero
+import org.apache.ignite.cache.CacheMode
+import org.apache.ignite.configuration.CacheConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import java.util.*
 import java.util.concurrent.TimeUnit
-import javax.annotation.PreDestroy
 import javax.cache.Cache
-import javax.cache.configuration.FactoryBuilder
+import javax.cache.CacheManager
+import javax.cache.Caching
 import javax.cache.expiry.AccessedExpiryPolicy
 import javax.cache.expiry.Duration
 import javax.cache.integration.CacheLoader
-import javax.cache.spi.CachingProvider
 
 
 @Component
 @ConditionalOnProperty(name = ["distributed-cache.enabled"], havingValue = "true")
-class DistributedOrderRepository(private val listener: ReplicatedMapEntryListener) : OrderRepository {
+class DistributedOrderRepository : OrderRepository {
     companion object {
-        val DIST_MAP_NAME = "distributedOrders"
-        val INSTANCE_NAME = "order-service-1"
         val JCACHE_NAME = "cachedOrders" + "-" + UUID.randomUUID().toString()
     }
 
-    private lateinit var instance: HazelcastInstance
     private lateinit var storage: Cache<String, Order>
 
     init {
-        val config = Config();
-        SubZero.useAsGlobalSerializer(config);
-        config.clusterName = "demo"
-        config.instanceName = INSTANCE_NAME
-        setupRestApiConfig(config)
-        setupDistributedMapConfig(config)
-
-        instance = Hazelcast.newHazelcastInstance(config)
-
-        storage = createCache(instance)
-    }
-
-    private fun createCache(instance: HazelcastInstance): Cache<String, Order> {
-        val provider: CachingProvider = HazelcastServerCachingProvider(instance);
-        val config: CacheConfig<String, Order> = CacheConfig(JCACHE_NAME)
+        val cacheMgr: CacheManager = Caching.getCachingProvider().getCacheManager()
+        val config: CacheConfiguration<String, Order> = CacheConfiguration()
+        config.setCacheMode(CacheMode.REPLICATED)
         config.setStoreByValue(false)
-        config.setReadThrough(true)
-        config.setCacheLoaderFactory(FactoryBuilder.SingletonFactory(cacheLoader()));
+        //config.setReadThrough(true)
+        //config.setCacheLoaderFactory(FactoryBuilder.SingletonFactory(cacheLoader()));
         config.setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(Duration(TimeUnit.MINUTES, 180)))
-        config.setInMemoryFormat(InMemoryFormat.OBJECT)
         config.isStatisticsEnabled = true
-        return provider.cacheManager.createCache(JCACHE_NAME, config)
+        storage = cacheMgr.createCache(JCACHE_NAME, config)
     }
 
     private fun cacheLoader(): CacheLoader<String, Order> {
@@ -71,19 +51,11 @@ class DistributedOrderRepository(private val listener: ReplicatedMapEntryListene
         }
     }
 
-    private fun setupRestApiConfig(config: Config) {
-        val restApiConfig = RestApiConfig()
-            .setEnabled(true)
-            .enableGroups(RestEndpointGroup.DATA)
-        config.networkConfig.restApiConfig = restApiConfig
-    }
-
     override fun save(order: Order): Order {
         if (storage.containsKey(order.id)) {
             throw IllegalArgumentException("Order with id ${order.id} already exist in Repository")
         }
         storage.put(order.id, order)
-        instance.getMap<String, Order>(DIST_MAP_NAME).put(order.id, order)
         return order
     }
 
@@ -93,19 +65,5 @@ class DistributedOrderRepository(private val listener: ReplicatedMapEntryListene
 
     override fun findAll(): Collection<Order> {
         return storage.asSequence().map { it.value }.toList()
-    }
-
-    private fun setupDistributedMapConfig(config: Config) {
-        val mapConfig = config.getMapConfig(DIST_MAP_NAME)
-        mapConfig.inMemoryFormat = InMemoryFormat.OBJECT
-        mapConfig.asyncBackupCount = 1
-        mapConfig.backupCount = 0
-        mapConfig.isReadBackupData = true
-        mapConfig.isStatisticsEnabled = true
-    }
-
-    @PreDestroy
-    fun destroy() {
-        instance.shutdown()
     }
 }
